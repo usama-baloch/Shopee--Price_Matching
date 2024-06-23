8bfa6cd586d1e3093331dbffcc22297e67ac3b34
usama baloch <usamabaloch152001@gmail.com>
2024-06-23 21:53:34 +0500

    update config file

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 train_images_path = ''
 train_csv_path = ''
+test_images_path = ''
+test_csv_path = ''
 
 IMG_SIZE = 512
 mean = [0.485, 0.456, 0.406]
@@ -7,15 +9,19 @@ std = [0.229, 0.224, 0.225]
 
 Epochs = 15
 BATCH_SIZE = 8
+BATCH_SIZE_VALID = 12
 
-num_workers = 4
+num_workers = 4 
 device = "cuda"
+seed = 2020
 
 classes = 11014 # number of unique classes in the dataset
 Scale = 30.0
 Margin = 0.5
 
 model_name = 'eca_nfnet_l0'
+model_path = ''
+
 FC_DIM = 512
 SCHEDULER_PARAMS = {
     "lr_start": 1e-5,
