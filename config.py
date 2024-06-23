60fa9a85ae5ac1d1ee1c99947b17ddbc733af0da
usama baloch <usamabaloch152001@gmail.com>
2024-06-23 18:06:47 +0500

    added config file

---
+++ config.py
@@ -0,0 +1,27 @@
+train_images_path = ''
+train_csv_path = ''
+
+IMG_SIZE = 512
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+
+Epochs = 15
+BATCH_SIZE = 8
+
+num_workers = 4
+device = "cuda"
+
+classes = 11014 # number of unique classes in the dataset
+Scale = 30.0
+Margin = 0.5
+
+model_name = 'eca_nfnet_l0'
+FC_DIM = 512
+SCHEDULER_PARAMS = {
+    "lr_start": 1e-5,
+    "lr_max": 1e-5 * 32,
+    "lr_min": 1e-6,
+    "lr_ramp_ep": 5,
+    "lr_sus_ep": 0,
+    "lr_decay": 0.8
+}
